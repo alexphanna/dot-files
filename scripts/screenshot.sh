736fed2ac985d512abc41d0ec3a4267e61219c9d
Alex Hanna <ahanna0608@gmail.com>
2024-06-28 12:49:03 -0400

    Add copying to scripts

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-mkdir -p ~/Pictures/Screenshots && scrot ~/Pictures/Screenshots/%m-%d-%Y-%H%M%S.png
+
+mkdir -p ~/Pictures/Screenshots
+scrot ~/Pictures/Screenshots/%m-%d-%Y-%H%M%S.png -e 'xclip -selection clipboard -target image/png -i $f'
